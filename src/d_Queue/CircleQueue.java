b11a2a31de5c66667875f45f90c4e5adda4570e9
“haha <zwqdddd@qq.com>
2021-11-18 09:24:12 +0800

    feature:用数组实现了循环队列

---
+++ src/d_Queue/CircleQueue.java
@@ -0,0 +1,77 @@
+package d_Queue;
+
+import a_ArrayList.ArrayListImpl;
+
+/**
+ * @Author qq
+ * @Date 2021/11/17
+ */
+public class CircleQueue<E> implements Queue<E>{
+    //循环队列，用数组来实现
+    private int front;//指示队列的头部
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+    private E[] elements = (E[]) new Object[DEFAULT_CAPACITY];
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public E deQueue() {
+        E element = elements[front];
+        elements[front] = null;
+        front = (front + 1) % elements.length;
+        size --;
+        return element;
+    }
+
+    @Override
+    public void enQueue(E element) {
+        ensureCapacity(size);
+        elements[(size + front)% elements.length] = element;
+        size ++;
+    }
+    private void ensureCapacity(int size){
+        if(size < elements.length) return;
+        int newCapacity = DEFAULT_CAPACITY + (DEFAULT_CAPACITY >> 1);
+        E[] newEelement = (E[]) new Object[newCapacity];
+        for(int i = 0; i < size; i++){
+            newEelement[i] = elements[(front + i)%elements.length];
+        }
+        elements = newEelement;
+        front = 0;
+    }
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public E front() {
+        return elements[front];
+    }
+
+    @Override
+    public E rear() {
+        return elements[(front + size)%elements.length - 1];
+    }
+    @Override
+    public String toString(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("size:" + size + " capacity:" + elements.length + " front:" + front + " [");
+        for(int i = 0; i < elements.length; i++){
+            if(i != 0){
+                sb.append(",");
+            }
+            sb.append(elements[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
--- src/d_Queue/test.java
+++ src/d_Queue/test.java
@@ -7,18 +7,18 @@ package d_Queue;
 public class test {
     public static void main(String args[]){
         //Queue<Integer> queue = new QueueImpl<>();
-        Queue<Integer> queue = new QueueByStack<>();
-        queue.enQueue(11);
-        queue.enQueue(22);
-        System.out.println("---"+queue.deQueue());
-        queue.enQueue(33);
-        queue.enQueue(44);
-        System.out.println("size:"+queue.size());
-        System.out.println("队首是"+queue.front());
-        System.out.println("队尾是"+queue.rear());
-        while (!queue.isEmpty()){
-            System.out.println(queue.deQueue());
-        }
+//        Queue<Integer> queue = new QueueByStack<>();
+//        queue.enQueue(11);
+//        queue.enQueue(22);
+//        System.out.println("---"+queue.deQueue());
+//        queue.enQueue(33);
+//        queue.enQueue(44);
+//        System.out.println("size:"+queue.size());
+//        System.out.println("队首是"+queue.front());
+//        System.out.println("队尾是"+queue.rear());
+//        while (!queue.isEmpty()){
+//            System.out.println(queue.deQueue());
+//        }
 
 
 //        ---11
@@ -31,5 +31,30 @@ public class test {
 //整体注释代码 Ctrl+/
 //整体右移代码 选中+tab
 
+        Queue<Integer> queue = new CircleQueue<>();
+        queue.enQueue(11);
+        queue.enQueue(22);
+        queue.enQueue(33);
+        queue.enQueue(44);
+        queue.enQueue(55);
+        queue.enQueue(66);
+        queue.enQueue(77);
+        queue.enQueue(88);
+        queue.enQueue(99);
+        queue.deQueue();
+        queue.deQueue();
+        //size:9 capacity:10 front:2 [null,null,33,44,55,66,77,88,99,null]
+
+        queue.enQueue(100);
+        queue.enQueue(110);
+        //size:9 capacity:10 front:2 [110,null,33,44,55,66,77,88,99,100]
+
+        queue.deQueue();
+        System.out.println(queue.front());
+        queue.enQueue(120);
+        queue.enQueue(130);
+        //size:11 capacity:15 front:0 [33,44,55,66,77,88,99,100,110,120,130,null,null,null,null]
+        System.out.println(queue.rear());
+        System.out.println(queue);
     }
 }
